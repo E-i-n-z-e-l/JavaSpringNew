779565f891d345ca2a82da0191c9523781ad802e
Einzel <einzel0102@mail.ru>
2024-03-19 14:32:54 +0800

    "Семинар №3. Задача №2."

---
+++ Seminar_03_Task_02/src/main/java/com/example/Seminar_03_Task_02/Seminar03Task02Application.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_03_Task_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar03Task02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar03Task02Application.class, args);
+	}
+
+}
