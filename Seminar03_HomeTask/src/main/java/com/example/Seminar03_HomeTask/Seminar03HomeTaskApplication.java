d3f9b9f07aca5afdfe714d59c1ecb43b483fd8aa
Einzel <einzel0102@mail.ru>
2024-03-19 15:36:07 +0800

    "Семинар №3. Задача №3."

---
+++ Seminar03_HomeTask/src/main/java/com/example/Seminar03_HomeTask/Seminar03HomeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Seminar03_HomeTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar03HomeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar03HomeTaskApplication.class, args);
+	}
+
+}
---
+++ Seminar03_HomeTask/src/test/java/com/example/Seminar03_HomeTask/Seminar03HomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar03_HomeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar03HomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
