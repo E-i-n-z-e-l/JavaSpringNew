958bfb0f54d57d7214fd45e35bab0e7b6d43b5db
Einzel <einzel0102@mail.ru>
2024-03-19 12:00:24 +0800

    "Семинар №3. Задача №1."

---
+++ Seminar_03_Task_01/src/test/java/com/example/Seminar_03_Task_01/Seminar03Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_03_Task_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar03Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
