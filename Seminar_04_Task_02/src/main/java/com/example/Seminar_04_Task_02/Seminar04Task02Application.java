63126227c1baa3c219861a82ca5ce873b037f1d1
Einzel <einzel0102@mail.ru>
2024-03-21 14:19:23 +0800

    "Семинар №4. Задача №2."

---
+++ Seminar_04_Task_02/src/main/java/com/example/Seminar_04_Task_02/Seminar04Task02Application.java
@@ -0,0 +1,11 @@
+package com.example.Seminar_04_Task_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar04Task02Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar04Task02Application.class, args);
+	}
+}
---
+++ Seminar_04_Task_02/src/main/java/com/example/Seminar_04_Task_02/controllers/RandomController.java
@@ -0,0 +1,17 @@
+package com.example.Seminar_04_Task_02.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Random;
+
+@Controller
+public class RandomController {
+    @GetMapping("/random")
+    public String getRandomNumber(Model model) {
+        Random random = new Random();
+        model.addAttribute("number", random.nextInt(100) + 1);
+        return "random";
+    }
+}
