96209f354cf3b3d2706f0d1d42b47f9b3d088f19
Einzel <einzel0102@mail.ru>
2024-03-22 10:58:02 +0800

    "Практическая №4. Задача №1."

---
+++ Practical_04_Task_01/src/main/java/com/example/Practical_04_Task_01/Practical04Task01Application.java
@@ -0,0 +1,13 @@
+package com.example.Practical_04_Task_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practical04Task01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practical04Task01Application.class, args);
+	}
+
+}
---
+++ Practical_04_Task_01/src/test/java/com/example/Practical_04_Task_01/Practical04Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practical_04_Task_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practical04Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
