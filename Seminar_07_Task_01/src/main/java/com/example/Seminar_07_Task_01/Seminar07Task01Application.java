87dee064cf02c9ab3fffe9343da39553a3abcb0e
Einzel <einzel0102@mail.ru>
2024-03-29 11:29:43 +0800

    "Семинар №7. Задача №1. Базовая защита. Не употребляется."

---
+++ Seminar_07_Task_01/src/main/java/com/example/Seminar_07_Task_01/Seminar07Task01Application.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_07_Task_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar07Task01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar07Task01Application.class, args);
+	}
+
+}
---
+++ Seminar_07_Task_01/src/test/java/com/example/Seminar_07_Task_01/Seminar07Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_07_Task_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar07Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
