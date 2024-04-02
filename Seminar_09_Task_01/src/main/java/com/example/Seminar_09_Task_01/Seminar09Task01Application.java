2f231579e129be309d41c29afcc87a5758cadc04
Einzel <einzel0102@mail.ru>
2024-04-02 14:09:51 +0800

    "Семинар №9. Не сделан"

---
+++ Seminar_09_Task_01/src/main/java/com/example/Seminar_09_Task_01/Seminar09Task01Application.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_09_Task_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar09Task01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar09Task01Application.class, args);
+	}
+
+}
---
+++ Seminar_09_Task_01/src/test/java/com/example/Seminar_09_Task_01/Seminar09Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar_09_Task_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar09Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
