29cc2f39fcd904993ec89f99ce48e81f15dfc97f
Einzel <einzel0102@mail.ru>
2024-04-05 10:43:40 +0800

    "Задание №10."

--- Practical_09_04/Practical_09_ApiGatewayBook/src/main/java/com/example/Practical_09_ApiGatewayBook/Practical09ApiGatewayBookApplication.java
+++ Practical_09_04/Practical_09_ApiGatewayBook/src/main/java/com/example/Practical_09_ApiGatewayBook/Practical09ApiGatewayBookApplication.java
@@ -16,9 +16,9 @@ public class Practical09ApiGatewayBookApplication {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("Microservice1", r -> r.path("/serviceA/**")
+                .route("FirstBook", r -> r.path("/serviceA/**")
                         .uri("http://localhost:8081/"))
-                .route("Microservice2", r -> r.path("/serviceB/**")
+                .route("SecondBook", r -> r.path("/serviceB/**")
                         .uri("http://localhost:8082/")).build();
     }
 
