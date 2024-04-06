a1a216adedc875dd094fc91e2253942f8902a7e1
Einzel <einzel0102@mail.ru>
2024-04-06 13:44:45 +0800

    "Задание №11."

---
+++ Practical_11/src/main/java/com/example/Practical_11/Practical11Application.java
@@ -0,0 +1,13 @@
+package com.example.Practical_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practical11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practical11Application.class, args);
+	}
+
+}
