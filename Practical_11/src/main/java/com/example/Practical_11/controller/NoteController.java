265d0c7c36ed48f03af2ad271189858320eed0f8
Einzel <einzel0102@mail.ru>
2024-04-06 15:22:16 +0800

    "Задание №11."

--- Practical_11/src/main/java/com/example/Practical_11/controller/NoteController.java
+++ Practical_11/src/main/java/com/example/Practical_11/controller/NoteController.java
@@ -2,6 +2,8 @@ package com.example.Practical_11.controller;
 
 import com.example.Practical_11.model.Note;
 import com.example.Practical_11.service.NoteService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,8 @@ import java.util.List;
 
 public class NoteController {
     private final NoteService noteService;
+    private final Counter requestCounter = Metrics.counter("request_count");
+    private final Counter requestCounter2 = Metrics.counter("request_count");
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
@@ -29,6 +33,7 @@ public class NoteController {
     }
     @PostMapping
     public ResponseEntity<Note> createNote(@RequestBody Note note) {
+        requestCounter.increment();
         Note createdNote = noteService.createNote(note);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdNote);
     }
@@ -43,6 +48,7 @@ public class NoteController {
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
+        requestCounter2.increment();
         noteService.deleteNoteById(id);
         return ResponseEntity.noContent().build();
     }
